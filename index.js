09945bd43195f0419060fbd20b060fe7c3230168
Joy <joyglory213@gmail.com>
2023-09-22 17:18:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  const thankYouMessages = [];
+
+  for (const name of names) {
+    thankYouMessages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+
